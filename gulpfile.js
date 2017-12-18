676ef9a3179c81c22efc46e1c96169110806f137
Alanis <alan.tonozzi@gmail.com>
2017-12-18 15:24:15 -0200

    gulp com switch e ambiente feito

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,43 @@
 var gulp = require('gulp');
-var htmlmin = require('gulp-htmlmin');
 var sass = require('gulp-sass');
 var pug = require('gulp-pug');
 var cleanCSS = require('gulp-clean-css');
+var environment = process.env.ENV;
 
 gulp.task('build:html', function(){
-	return gulp.src('src/index.pug')
-		.pipe(pug())
-		.pipe(htmlmin({collapseWhitespace: true}))
-		.pipe(gulp.dest('dist'));
+	switch (environment) {
+		case 'prod':
+			return gulp.src('src/index.pug')
+				.pipe(pug())
+				.pipe(gulp.dest('dist'));
+			break;
+		case 'dev':
+		default:
+			return gulp.src('src/index.pug')
+				.pipe(pug({
+					pretty: '\t'
+				}))
+				.pipe(gulp.dest('dist'));
+			break;
+	}
+
 });
 
 gulp.task('build:css', function(){
-	return gulp.src('src/main.sass')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(cleanCSS())
-		.pipe(gulp.dest('dist'));
+	switch (environment) {
+		case 'prod':
+			return gulp.src('src/main.sass')
+				.pipe(sass().on('error', sass.logError))
+				.pipe(cleanCSS())
+				.pipe(gulp.dest('dist'));
+			break;
+		case 'dev':
+		default:
+			return gulp.src('src/main.sass')
+				.pipe(sass().on('error', sass.logError))
+				.pipe(gulp.dest('dist'));
+			break;
+	}
 });
 
 gulp.task('build', ['build:html', 'build:css']);
